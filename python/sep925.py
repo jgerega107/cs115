0cec61ad569aa2130a1e788e5c6d2bbf61ced9f8
jgerega107 <aftertastephd@gmail.com>
2019-09-25 11:24:36 -0400

    HW3

---
+++ python/sep925.py
@@ -0,0 +1,23 @@
+#first function call in a recursion problem is not considered a recursive call, this is for when you get a problem that asks how many recursive calls.
+
+#multiple recursion is called TREE recursion
+#single recursion is called LINEAR recursion ONLY IF THERE IS COMPUTATION AFTER THE RETURN
+#otherwise, it is called tail recursion if there is no computation
+
+#REVIEW YOUR DEEP REVERSE FUNCTION!!!!!!
+
+"""
+PROBLEM 6
+Assume lst is a list of values, some of which may also be lists. Returns a list with all the values collapsed in reverse order. In other words,
+nesting has been removed and the values as they appear from left to right in the input list are now returned from right to left. You may
+not use negative indexing or slicing.
+"""
+
+"""
+PROBLEM 7
+Assume values is a list of integers that are the squares of the original values
+for those original values that are less than the threshold."""
+
+#^^^SEE THE ANSWERS ON CANVAS
+
+
