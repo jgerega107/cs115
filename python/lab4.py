76660cc4586b5412dc14004df396d02ca9bc0b28
after <aftertastephd@gmail.com>
2019-09-26 18:29:31 -0400

    More progress on function 2

--- python/lab4.py
+++ python/lab4.py
@@ -5,15 +5,17 @@ def coin_row(coins):
         return coins[0]
     else:
         evenIndexes = coins[0] + coin_row(coins[2::])
-        oddIndexes = coins[1] + coin_row(coins[3::])
+        oddIndexes = coin_row(coins[1::])
         return max(evenIndexes, oddIndexes)
 
 def coin_row_with_values(coins):
     if coins == []:
-        return 0
-    elif len(coins) == 1:
-        return coins[0]
+        return [0, []]
     else:
-        evenIndexes = coins[0] + coin_row(coins[2::])
-        oddIndexes = coins[1] + coin_row(coins[3::])
-        return max(evenIndexes, oddIndexes)
+        evenIndexes = coin_row_with_values(coins[2::])
+        oddIndexes = coin_row_with_values(coins[1::])
+        maximum = max(evenIndexes[0], oddIndexes[0])
+        if maximum == evenIndexes[0]:
+            return [evenIndexes[0], [coins[0]] + evenIndexes[1]]
+        else:
+            return [oddIndexes[0], [coins[0]] + oddIndexes[1]]
