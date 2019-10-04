83b5c0a24d744e8b05e86cfc087e97877505ac99
after <aftertastephd@gmail.com>
2019-10-04 09:24:47 -0400

    Added hw4

---
+++ python/hw/hw4.py
@@ -0,0 +1,33 @@
+'''
+Created on 10/4/19
+@author:   Jacob Gerega
+Pledge:    I pledge my honor that I have abided by the Stevens Honor System
+
+CS115 - Hw 5
+'''
+import turtle  # Needed for graphics
+
+# Ignore 'Undefined variable from import' errors in Eclipse.
+
+def snowflake(trunk_length, levels):
+    pass  # TODO
+
+def fast_change(amount, coins):
+    '''Takes an amount and a list of coin denominations as input.
+    Returns the number of coins required to total the given amount.
+    Use memoization to improve performance.'''
+    def fast_change_helper(amount, coins, memo):
+        pass  # Write your code here
+
+    # Call the helper. Note we converted the list to a tuple.
+    return fast_change_helper(amount, tuple(coins), {})
+
+# If you did this correctly, the results should be nearly instantaneous.
+print(fast_change(131, [1, 5, 10, 20, 50, 100]))
+print(fast_change(292, [1, 5, 10, 20, 50, 100]))
+print(fast_change(673, [1, 5, 10, 20, 50, 100]))
+print(fast_change(724, [1, 5, 10, 20, 50, 100]))
+print(fast_change(888, [1, 5, 10, 20, 50, 100]))
+
+# Should take a few seconds to draw a snow flake
+snowflake(800, 3)
