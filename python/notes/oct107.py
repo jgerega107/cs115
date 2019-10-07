9c661e5e41205b6a42c0e60623b8630daf00d3db
after <aftertastephd@gmail.com>
2019-10-07 11:23:07 -0400

    Added 10/7 notes

---
+++ python/notes/oct107.py
@@ -0,0 +1,25 @@
+"""
+when converting decimal to binary, you stop when the quotient is 2 or less
+
+convert base 10 to any base
+25 % base = remainder
+25 / base = new quotient
+
+then take new quotient and get new remainder and new quotient
+
+
+25 % 2 = 1
+25 / 2 = 12
+
+2^0 = 1
+2^1 = 0
+2^2 = 0
+and so on
+
+change 2 to any base to convert to given bases
+
+when we add in base 10, when the row goes over or to 10, we carry over to next place
+when we add in base 2(binary), when the row goes over or to 2, we carry over to next place
+
+
+"""
