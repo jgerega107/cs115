f93e3c3cb023ec6e8ceff6a7076f65032a151c18
Jacob Gerega <22921310+jgerega107@users.noreply.github.com>
2019-10-24 18:18:22 -0400

    Added comments

--- python/hw/hw6.py
+++ python/hw/hw6.py
@@ -16,35 +16,40 @@ FullAdder = { ('0','0','0') : ('0','0'),
 ('1','1','0') : ('0','1'),
 ('1','1','1') : ('1','1') }
 
+#numToBaseB: takes in a base 10 number N and a base B and returns N in base B
 def numToBaseB(N, B):
     if N <= 0:
         return ''
     else:
         return numToBaseB(N // B, B) + str(N % B)
 
+#baseBToNum: takes in a base B number and a base B and returns a number in base 10
 def baseBToNum(N, B):
     if len(N) <= 0:
         return 0
     else:
         return int(int(N[0]) * math.pow(B, len(N)-1)) + baseBToNum(N[1:], B)
 
+#baseToBase: takes in a number of base B1 and returns a number of base B2
 def baseToBase(B1, B2, SinB1):
     decimal = baseBToNum(SinB1, B1)
     return numToBaseB(decimal, B2)
 
+#add: takes in two base 2 numbers and returns the result of adding them together while still keeping base
 def add(N1, N2):
     n1dec = baseBToNum(N1, 2)
     n2dec = baseBToNum(N2, 2)
     sum = n1dec + n2dec
     return numToBaseB(sum, 2)
 
+#addB: takes in two base 2 numbers and returns the result of adding them together while still keeping base (without converting to decimal mid process)
 def addB(N1, N2):
     if not N1 and not N2:
         return "0"
     else:
         return addWithCarry(N1, N2, "0")
 
-
+#addWithCarry: helper function for addB, allows extra carryIn parameter to mimic a full adder circuit
 def addWithCarry(N1, N2, carryIn):
     if N1 == "0" and not N2:
         return carryIn
