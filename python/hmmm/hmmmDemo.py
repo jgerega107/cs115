7a3879cdddba9a83f2d0ad94cd4158e674ea86a1
Jacob Gerega <22921310+jgerega107@users.noreply.github.com>
2019-10-31 02:00:37 -0400

    HMMM assembly lab completed

---
+++ python/hmmm/hmmmDemo.py
@@ -0,0 +1,49 @@
+# Demo of hmmm, the Harvey Mudd miniature machine
+# D.N. Oct 2018
+
+# When this file is loaded, it runs the program assigned
+# to variable RunThis. Debug mode is controlled by 
+# variable Mode. Read all the comments before trying it out.
+
+import sys
+import importlib
+# Also requires hmmmAssembler.py and hmmmSimulator.py to
+# be available in the same directory as this file.
+
+fibonacci = """
+00 read r1
+01 setn r2 0
+02 setn r3 1
+03 jeqzn r1 10
+04 write r2
+05 add r4 r3 r2
+06 copy r2 r3
+07 copy r3 r4
+08 addn r1 -1
+09 jumpn 3
+10 halt
+"""
+
+
+
+# Set this variable to whichever program you want to execute
+# when this file is loaded.
+RunThis = fibonacci
+
+# Choose whether to use debug mode; uncomment one of the following lines.
+#Mode = ['-n'] # not debug mode
+Mode = ['-d'] # debug mode
+#Mode = []     # prompt for whether to enter debug mode
+
+
+# When you press F5 in IDLE, the following code will
+# load the assembler and simulator, then run them.
+# You can interrupt with Ctrl-C; then re-start Python.
+
+if __name__ == "__main__" : 
+    import hmmmAssembler ; importlib.reload(hmmmAssembler)
+    import hmmmSimulator ; importlib.reload(hmmmSimulator)
+    hmmmAssembler.main(RunThis) # assemble input into machine code file out.b
+    hmmmSimulator.main(Mode)    # run the machine code in out.b
+
+
--- python/hw/hw7.py
+++ python/hw/hw7.py
@@ -48,12 +48,11 @@ def fillBits(N):
 
 def NumToTc(num):
     if num < 0:
-        binaryStr = fillBits(numToBaseB(num*-1, 2))
-        tc = add(TcToNumHelper(binaryStr), '1')
+        binaryStr = numToBaseB(num*-1, 2)
+        tc = fillBits(add(TcToNumHelper(binaryStr), '1'))
         return tc
     else:
         binaryStr = fillBits(numToBaseB(num, 2))
         return binaryStr
 
-
-print(NumToTc(-129))
+print(NumToTc(255))
