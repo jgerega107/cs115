d6b8b89351f01bc52bfda6143a1d8303b0276b8b
Jacob Gerega <22921310+jgerega107@users.noreply.github.com>
2019-11-19 03:45:29 -0500

    Added date homework

---
+++ python/hw/date.py
@@ -0,0 +1,48 @@
+'''
+Created on 11/19/2019
+@author:   Jacob Gerega
+Pledge:    I pledge my honor that I have abided by the Stevens Honor System.
+
+CS115 - Hw 10 - Date class
+'''
+DAYS_IN_MONTH = (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+class Date(object):
+    '''A user-defined data structure that stores and manipulates dates.'''
+
+    # The constructor is always named __init__.
+    def __init__(self, month, day, year):
+        '''The constructor for objects of type Date.'''
+        self.month = month
+        self.day = day
+        self.year = year
+
+    # The 'printing' function is always named __str__.
+    def __str__(self):
+        '''This method returns a string representation for the
+           object of type Date that calls it (named self).
+
+             ** Note that this _can_ be called explicitly, but
+                it more often is used implicitly via the print
+                statement or simply by expressing self's value.'''
+        return '%02d/%02d/%04d' % (self.month, self.day, self.year)
+
+    # Here is an example of a 'method' of the Date class.
+    def isLeapYear(self):
+        '''Returns True if the calling object is in a leap year; False
+        otherwise.'''
+        if self.year % 400 == 0:
+            return True
+        if self.year % 100 == 0:
+            return False
+        if self.year % 4 == 0:
+            return True
+        return False
+
+    def copy(self):
+        '''Returns a new object with the same month, day, year
+        as the calling object (self).'''
+        dnew = Date(self.month, self.day, self.year)
+        return dnew
+
+
